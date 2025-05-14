f40d6e56d8134b187721eaacb5e74fd2864ae682
IgorCzudy <iczudy@gmail.com>
2025-05-14 13:28:50 +0200

    feat: full ETL pipeline

--- dags/mainETL.py
+++ dags/mainETL.py
@@ -1,55 +1,53 @@
 from airflow import DAG
 from datetime import datetime, timedelta
 import pytz
-from airflow.operators.python import PythonOperator
 import praw
 import os
 import urllib.request
 from airflow.decorators import task
-from minio import Minio
 from urllib.parse import urlsplit
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from airflow.hooks.base import BaseHook
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 import tempfile
 
 
+
 with DAG(
     dag_id="reddit_test",
-    start_date=datetime(2023, 1, 1),
-    schedule=None,  # manual run
-    catchup=False,
+    start_date=datetime(2025, 5, 12, 8, 0),
+    schedule="@daily", 
+    catchup=True,
     tags=["example"]
 ) as dag:
 
     @task()
-    def fetch_reddit_posts():
+    def fetch_reddit_posts(**kwargs):
         reddit = praw.Reddit(
         client_id=os.environ.get('REDDIT_CLIENT_ID'),
         client_secret=os.environ.get('REDDIT_CLIENT_SECRET'),
         user_agent=os.environ.get('REDDIT_USER')
         )
-        
         utc = pytz.UTC
-        now = datetime.now(utc)
-        one_day_ago = now - timedelta(days=1)
+        context = kwargs
+        execution_date = context["logical_date"]  # Start of interval (UTC)
+        one_day_ago = execution_date - timedelta(days=1)
 
         posts = []
         for subredit in ["ProgrammerHumor", "programminghumor", "programmingmemes", "DataScienceMemes"]:
             for submission in reddit.subreddit(subredit).new():
                 post_time = datetime.fromtimestamp(submission.created_utc, tz=utc)
-                
-                if one_day_ago <= post_time:
+
+                if one_day_ago <= post_time < execution_date :
                     if submission.url.endswith(('.jpg', '.jpeg', '.png', '.gif')):
                         posts.append({
                             "id": submission.id,
                             "url": submission.url,
                             "title": submission.title,
                             "score": submission.score,
-                            "created_utc": submission.created_utc,
+                            "created_utc": post_time,
                             "author": submission.author.name if submission.author else "Unknown",
                             "num_comments": submission.num_comments,
-                            "post_time_iso": post_time.isoformat()  # Convert datetime to string
+                            "execution_time": execution_date
                         })
         return posts
     
@@ -59,7 +57,13 @@ with DAG(
         transformed_posts = []
         for post in posts:
             if post["score"] > 50:
+                url = post["url"]
+                path = urlsplit(url).path
+                extension = os.path.splitext(path)[1][1:]
+                s3_url = f"memes/{post['id']}.{extension}"
+                post["s3_url"] = s3_url
                 transformed_posts.append(post)
+                
 
         return transformed_posts
 
@@ -69,18 +73,14 @@ with DAG(
 
         miniio_hook = S3Hook(aws_conn_id="minio_conn_s3")
 
-        for submission in posts:
+        for post in posts:
             try:
-                url = submission["url"]
-                path = urlsplit(url).path
-                extension = os.path.splitext(path)[1][1:]  # Extracts "jpg" from "/path/image.jpg"
-                filename = f"memes/{submission['id']}.{extension}"
-                with urllib.request.urlopen(submission["url"]) as response:
+                with urllib.request.urlopen(post["url"]) as response:
                     with tempfile.NamedTemporaryFile() as tmp:
                         tmp.write(response.read())
                         miniio_hook.load_file(
                             filename=tmp.name,
-                            key=filename,
+                            key=post["s3_url"],
                             bucket_name='memes',
                             replace=True
                         )
@@ -97,24 +97,34 @@ with DAG(
         cursor = conn.cursor()
 
         cursor.execute("""
-        CREATE TABLE IF NOT EXISTS memes_test(
-                id VARCHAR PRIMARY KEY,
-                url TEXT,
-                title TEXT,
-                score INT
+        CREATE TABLE IF NOT EXISTS memes(
+                    id SERIAL PRIMARY KEY,
+                    meme_id TEXT,
+                    title TEXT NOT NULL,
+                    score INTEGER,
+                    created_utc TIMESTAMP WITH TIME ZONE,
+                    s3_url TEXT,
+                    author VARCHAR(50),
+                    num_comments INTEGER,
+                    execution_time TIMESTAMP WITH TIME ZONE
             );
             """)
-        
 
         for post in posts:
+
             cursor.execute("""
-            INSERT INTO memes_test (id, url, title, score)
-            VALUES (%s, %s, %s, %s)
+            INSERT INTO memes (meme_id, title, score, created_utc, s3_url, author, num_comments, execution_time)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+            ON CONFLICT (id) DO NOTHING
             """,(
                 post["id"],
-                post["url"],
                 post["title"],
                 post["score"],
+                post["created_utc"],
+                post["s3_url"],
+                post["author"],
+                post["num_comments"],
+                post["execution_time"],
             ))
         
         conn.commit()
@@ -127,12 +137,3 @@ with DAG(
     load_memes_to_DB(transf_posts)
 
 
-
-    # ...: client = tweepy.Client(
-    # ...:     bearer_token="AAAAAAAAAAAAAAAAAAAAAK7Y1AEAAAAASxGU3vCcHKpqTz6vQ0hwa72aSyk%3DXK2CafDshSRKpTiiS1d1p20gVpasZ0Pr9L
-    # ...: RAHxWnGbr0jWyxbR",
-    # ...:     consumer_key="Rh1PxqS3G3nuEyTXp3JhzrrBH",
-    # ...:     consumer_secret="uigV9aiLBcLc757mE5iyaJbTHbe3AzuCUwMenqQsDTk1FefvFQ",
-    # ...:     access_token="1921856203052781569-vrMs995p51B51rgqkbbQ4zr7iPBFz3",
-    # ...:     access_token_secret="6lGc09lVcu2ITJ34O80PTuR28rIFtHOqee8ysJkk5KYXk"
-    # ...: )
